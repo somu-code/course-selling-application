e59aaf6bc39d3f9d0511744a5195bb7776617c94
Somnath Golui <code.somu@gmail.com>
2023-08-08 16:52:51 +0530

    recoil now updates the state successfully

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { adminEmailState } from "../store/selectors/adminEmail";
+import { isAuthenticated } from "../store/atoms/authenticated";
 
 function Navbar() {
   const adminEmail = useRecoilValue(adminEmailState);
+  const auth = useRecoilValue(isAuthenticated);
   return (
     <header className="">
       <nav className="grid grid-cols-[1fr_auto_1fr] grid-rows-[auto] items-center font-medium px-6 py-3">
@@ -22,8 +24,8 @@ function Navbar() {
           </Link>
         </div>
         <div className="flex flex-row items-center justify-end gap-2">
-          {adminEmail ? (
-            <h2 className="text-lg">{adminEmail}</h2>
+          {auth ? (
+            <h2 className="text-lg">accounts</h2>
           ) : (
             <Link to="/admin/signup">
               <button className="text-[#151439] text-lg">Sign Up</button>
