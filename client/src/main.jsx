77fb5172717f037ea9d249add0aa7a4b41f49ac2
Somnath Golui <code.somu@gmail.com>
2023-08-10 10:53:58 +0530

    Working with react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RecoilRoot } from "recoil";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RecoilRoot>
       <BrowserRouter>
-        <App />
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
       </BrowserRouter>
     </RecoilRoot>
   </React.StrictMode>
