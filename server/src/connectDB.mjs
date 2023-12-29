e5ee5d2a01751bcb074c5ccb8046588476bae847
Somnath Golui <code.somu@gmail.com>
2023-12-29 13:09:49 +0530

    Edited: dotenv.config and added a ping api

--- server/src/connectDB.mjs
+++ server/src/connectDB.mjs
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.mongoURI, {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
     console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(error.message);
-      process.exit(1);
+    process.exit(1);
   }
 };
