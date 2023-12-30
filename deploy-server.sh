585b0ae0e4e804b3f6ede1901f04c08daa2929fe
Somnath Golui <code.somu@gmail.com>
2023-12-30 15:27:01 +0530

    Added deployment script

---
+++ deploy-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.10.0/bin
+
+cd coursera-javascript
+git pull origin main
+cd server
+pm2 kill
+npm install
+pm2 start ./src/index.mjs
