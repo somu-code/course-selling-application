a0b7ee81b1b88134ffd45de9f28da791da1eff1c
Somnath Golui <code.somu@gmail.com>
2023-12-30 15:37:34 +0530

    test

--- server/src/routes/user.mjs
+++ server/src/routes/user.mjs
@@ -5,7 +5,7 @@ export const userRouter = express.Router();
 userRouter.get("/", async (req, res) => {
   try {
     const data = req.body;
-    res.json({ message: "Hello, world!", data });
+    res.json({ message: "CD working via github actions", data });
   } catch (error) {
     res.sendStatus(500);
   }
