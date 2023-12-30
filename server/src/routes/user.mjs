7836bb65177e4e50bab55ae2b9b083d50559f773
Somnath Golui <code.somu@gmail.com>
2023-12-30 14:31:51 +0530

    update

--- server/src/routes/user.mjs
+++ server/src/routes/user.mjs
@@ -5,7 +5,7 @@ export const userRouter = express.Router();
 userRouter.get("/", async (req, res) => {
   try {
     const data = req.body;
-    res.json({ message: "Hello, user", data });
+    res.json({ message: "Hello, world!", data });
   } catch (error) {
     res.sendStatus(500);
   }
