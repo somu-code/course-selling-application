5c7237f157adc879f238c5f12438231fb9353415
Somnath Golui <code.somu@gmail.com>
2024-01-10 02:04:36 +0530

    testing prettier workflow

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -54,6 +54,17 @@ userRouter.post("/signin", async (req, res) => {
 userRouter.get("/", async (req, res) => {
   try {
     const data = req.body;
+
+
+
+
+
+
+
+
+
+
+
     res.json({ message: "CD working via github actions", data });
   } catch (error) {
     res.sendStatus(500);
