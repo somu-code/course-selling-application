0d2a7cc8fa9519ac4c8e83766c08e2e85cd91edd
Somnath Golui <code.somu@gmail.com>
2024-01-10 14:17:47 +0530

    Formated: user.ts

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -55,16 +55,6 @@ userRouter.get("/", async (req, res) => {
   try {
     const data = req.body;
 
-
-
-
-
-
-
-
-
-
-
     res.json({ message: "CD working via github actions", data });
   } catch (error) {
     res.sendStatus(500);
