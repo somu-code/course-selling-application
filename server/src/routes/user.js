51456aa0ce9d200764f588baf117c99f96bc9c6d
Somnath Golui <code.somu@gmail.com>
2024-01-10 15:47:23 +0530

    Created: function authenticatedUserJWT. Created: routes /profile, /logout, /delete-account, and /courses for user

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { User } from "../models/user-model.js";
 import bcrypt from "bcrypt";
-import { generateUserJWT } from "../jwt-auth/user-auth.js";
+import { authenticateUserJWT, generateUserJWT } from "../jwt-auth/user-auth.js";
+import Course from "../models/course-model.js";
 
 export const userRouter = Router();
 
@@ -35,7 +36,7 @@ userRouter.post("/signin", async (req, res) => {
     if (!isPasswordMatch) {
       return res.status(401).json({ message: "Invalid password" });
     }
-    const userPayload = { id: userData.id, email: userData.email };
+    const userPayload = { _id: userData._id, email: userData.email };
     const userToken = generateUserJWT(userPayload);
     res.cookie("userAccessToken", userToken, {
       domain: "localhost",
@@ -51,12 +52,46 @@ userRouter.post("/signin", async (req, res) => {
   }
 });
 
-userRouter.get("/", async (req, res) => {
+userRouter.get("/profile", authenticateUserJWT, async (req, res) => {
   try {
-    const data = req.body;
-
-    res.json({ message: "CD working via github actions", data });
+    const user = req.user;
+    console.log(user);
+    const userData = await User.find({ _id: user._id });
+    return res.json(userData);
   } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+})
+
+userRouter.post("/logout", authenticateUserJWT, async (_req, res) => {
+  try {
+    res.clearCookie("userAccessToken");
+    res.json({ message: "Logged out successfully" })
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+})
+
+userRouter.delete("/delete-account", authenticateUserJWT, async (req, res) => {
+  try {
+    const user = req.user;
+    await User.findOneAndDelete({ _id: user._id })
+    res.clearCookie("userAccessToken");
+    res.json({ message: "User account deleted successfully" })
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
+
+userRouter.get("/courses", authenticateUserJWT, async (_req, res) => {
+  try {
+    const courses = await Course.find({});
+    res.json(courses);
+  } catch (error) {
+    console.error(error);
     res.sendStatus(500);
   }
 });
