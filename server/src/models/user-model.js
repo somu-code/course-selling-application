55aa84b2aaba34d6d5c20e51447602e47801910e
Somnath Golui <code.somu@gmail.com>
2024-01-10 16:09:21 +0530

    Updated: user and admin model

--- server/src/models/user-model.js
+++ server/src/models/user-model.js
@@ -1,8 +1,22 @@
 import mongoose from "mongoose";
 
 const userSignupSchema = new mongoose.Schema({
-  email: String,
-  password: String,
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  role: {
+    type: String,
+    default: "user"
+  }
 });
 
 export const User = mongoose.model("User", userSignupSchema);
